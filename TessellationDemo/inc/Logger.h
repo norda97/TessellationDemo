f4bd66b85032200479e2cd899906717c901f4f56
Adrian <adde--97@hotmail.com>
2020-12-07 21:58:09 +0100

    Added console color

--- TessellationDemo/inc/Logger.h
+++ TessellationDemo/inc/Logger.h
@@ -2,5 +2,12 @@
 
 #include <iostream>
 
-#define LOG_ERROR(errorMsg, ...) printf("LOG_ERROR: "); printf(errorMsg, __VA_ARGS__); printf("\n");
-#define LOG_INFO(infoMsg, ...) printf("LOG_INFO: "); printf(infoMsg, __VA_ARGS__); printf("\n");
+#ifdef _WIN32
+#include <Windows.h>
+#define CHANGE_CONSOLE_ATTRIBUTE(attribute) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), attribute)
+#elif
+#define CHANGE_CONSOLE_ATTRIBUTE(attribute)
+#endif
+
+#define LOG_ERROR(errorMsg, ...) CHANGE_CONSOLE_ATTRIBUTE(12); printf("LOG_ERROR: "); printf(errorMsg, __VA_ARGS__); printf("\n"); CHANGE_CONSOLE_ATTRIBUTE(15);
+#define LOG_INFO(infoMsg, ...) CHANGE_CONSOLE_ATTRIBUTE(10); printf("LOG_INFO: "); printf(infoMsg, __VA_ARGS__); printf("\n"); CHANGE_CONSOLE_ATTRIBUTE(15);
